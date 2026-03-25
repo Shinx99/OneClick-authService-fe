71bd8979450470026c713bfd132366a48e0661e5
HoangVuongBui <buihoangvuong777@gmail.com>
2026-03-25 18:18:58 +0700

    Fixed login bugs - #48

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./globals.css";
+import { AuthProvider } from "@/context/AuthContext";
 
 // Cấu hình Font chữ
 const inter = Inter({
@@ -26,47 +27,52 @@ export default function RootLayout({ children }) {
       >
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
 
-          {/* TOASTER ĐẶT Ở ĐÂY ĐỂ TRANG NÀO CŨNG GỌI ĐƯỢC */}
-          <Toaster
-            position="top-right"
-            reverseOrder={false}
-            toastOptions={{
-              className: "",
-              style: {
-                background: "#ffffff",
-                color: "#334155",
-                fontFamily: "inherit",
-                fontSize: "14px",
-                fontWeight: "600",
-                padding: "16px 20px",
-                borderRadius: "16px",
-                boxShadow:
-                  "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 8px 10px -6px rgba(0, 0, 0, 0.1)",
-                border: "1px solid #f1f5f9",
-              },
-              success: {
-                duration: 3000,
-                iconTheme: {
-                  primary: "#10B94F",
-                  secondary: "#ffffff",
-                },
-              },
-              error: {
-                duration: 4000,
-                iconTheme: {
-                  primary: "#ef4444",
-                  secondary: "#ffffff",
-                },
-              },
-            }}
-            containerStyle={{
-              top: 100,
-              right: 20,
-            }}
-          />
+          <AuthProvider>
 
-          {/* Nơi nhúng các Layout con */}
-          {children}
+            {/* TOASTER ĐẶT Ở ĐÂY ĐỂ TRANG NÀO CŨNG GỌI ĐƯỢC */}
+            <Toaster
+              position="top-right"
+              reverseOrder={false}
+              toastOptions={{
+                className: "",
+                style: {
+                  background: "#ffffff",
+                  color: "#334155",
+                  fontFamily: "inherit",
+                  fontSize: "14px",
+                  fontWeight: "600",
+                  padding: "16px 20px",
+                  borderRadius: "16px",
+                  boxShadow:
+                    "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 8px 10px -6px rgba(0, 0, 0, 0.1)",
+                  border: "1px solid #f1f5f9",
+                },
+                success: {
+                  duration: 3000,
+                  iconTheme: {
+                    primary: "#10B94F",
+                    secondary: "#ffffff",
+                  },
+                },
+                error: {
+                  duration: 4000,
+                  iconTheme: {
+                    primary: "#ef4444",
+                    secondary: "#ffffff",
+                  },
+                },
+              }}
+              containerStyle={{
+                top: 100,
+                right: 20,
+              }}
+            />
+
+            {/* Nơi nhúng các Layout con */}
+            {children}
+
+
+          </AuthProvider>
         </GoogleOAuthProvider>
       </body>
     </html>
